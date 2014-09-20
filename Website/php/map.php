157c3a434455f1b039d726844134a11c5696ca8a
Cristian Frincu <cristianf@me.com>
2014-09-20 04:47:46 -0400

    does not display circles but function is called
    map shows up properly

--- Website/php/map.php
+++ Website/php/map.php
@@ -1,30 +1,30 @@
 <?php
-		$connection = mysqli_connect("mysql.freehostingnoads.net","u476951036_cris","HQz8XvH3ZC","u476951036_login");     
+	$connection = mysqli_connect("mysql.freehostingnoads.net","u476951036_cris","HQz8XvH3ZC","u476951036_login");     
 	if (mysqli_connect_errno()) {
 	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	}     
 	
-	$result = mysqli_query($connection,"SELECT * FROM Slices ORDER BY Date DESC"); 
+
+	include_once("../displayMap.html");
+
+	$result = mysqli_query($connection,"SELECT * FROM Slices"); 
+
 	$userXPos=$_GET['xPos'];
 	$userYPos=$_GET['yPos'];
-	
-	$DEFAULT_SEARCH_RADIUS=1000;
-	
+	$DEFAULT_SEARCH_RADIUS=1000000;
+	echo "<script>"; // will be responsible of calling the functions to draw each event
 	while($row = mysqli_fetch_array($result)) {
 		$eventXPos=$row['XPos'];
 		$eventYPos=$row['YPos'];
-		if(($eventXPos <= ($userX+$DEFAULT_SEARCH_RADIUS))&&($eventXPos<=($userXPos-$DEFAULT_SEARCH_RADIUS))&&($eventYPos<=($userYPos+$DEFAULT_SEARCH_RADIUS))&&($eventYPos<=($userYPos-$DEFAULT_SEARCH_RADIUS))){
-			$distanceX=$eventXPos-$userXPos;
-			$distanceY=$eventYPos-$userYPos;
-			$distanceR=sqrt(pow($distanceX,2)+pow($distanceY,2));
-			
+		$distanceX=$eventXPos-$userXPos;
+		$distanceY=$eventYPos-$userYPos;
+		$distanceR=sqrt(pow($distanceX,2)+pow($distanceY,2));
+		echo "drawCircle2(".$eventXPos.",".$eventYPos.", 50000);";	
+		if($distanceR<$DEFAULT_SEARCH_RADIUS){
+			echo "drawCircle2(".$eventXPos.",".$eventYPos.", 50000);";	
 		}
-	}	
-
-
-	
-	
-	function isUserWithin($mFromEvent,$eventX,$eventY){
 		
-	}
+	}	
+	echo "</script>";
+
 ?>
